ceb66a02e888008ada68207c15a247eadcaac0f5
Brezniev Wakou <bdydkotomek@gmail.com>
2021-07-14 18:16:04 +0200

    Add Binary Files

---
+++ .uninstall.sh
@@ -0,0 +1,68 @@
+licz()
+{
+if [ -e /usr/bin/licz ]
+then
+rm -f /usr/bin/licz
+else
+rm -f /bin/licz
+
+fi
+}
+
+conf()
+{
+rm -rf /etc/Toolkit
+}
+
+
+
+
+
+
+
+odinst()
+{
+mv .install.sh install.sh
+mv uninstall.sh .uninstall.sh
+}
+
+DESTROY()
+{
+cd `find /home -type d -name "licz" 2>/dev/null`
+cd ..
+cd ..
+rm -rf  $OLDPWD
+}
+
+
+
+
+
+
+
+
+sleep 1
+echo "Wybierz interesujaca cie opcje wpisujac cyfre z poczatku wiersza:"
+echo "1.Odinstaluj pakiet"
+echo "2.Odinstaluj pakiet i usun folder ze skryptem instalacyjnym"
+echo "3.Chce wyjsc z tego menu"
+
+read odpowiedz
+
+if [ $odpowiedz -eq 1 ];
+then
+echo "Odinstalowywanie calego pakietu..."
+licz;conf;odinst;
+elif [ $odpowiedz -eq 2 ];
+then
+licz;conf;DESTROY;
+elif [ $odpowiedz -eq 3 ];
+then
+echo "Wychodzenie..."
+sleep 0.7
+clear
+else
+echo "Co?"
+echo "Nie ma takiej opcji w menu!"
+
+fi
---
+++ install.sh
@@ -0,0 +1,75 @@
+inst()
+{
+if [ -e "/etc/Toolkit/" ]; then
+echo "Znaleziono folder z plikami konfiguracyjnymi"
+else
+echo "Tworzenie folderu z plikami konfiguracyjnymi"
+sudo mkdir /etc/Toolkit
+fi
+}
+
+
+
+
+pakiety()
+{
+echo "Instalowanie..."
+sudo mkdir /etc/Toolkit/licz
+sudo mv licz/code.licz.sh /etc/Toolkit/licz/code.licz.sh
+
+if [ -e /usr/bin ]; then
+sudo mv licz/licz /usr/bin/licz
+else
+sudo mv licz/licz /bin/licz
+fi
+}
+
+
+
+Install()
+{
+mv install.sh .install.sh
+mv .uninstall.sh uninstall.sh
+}
+
+
+
+
+
+
+
+
+echo "Witaj w instalatorze mojego toolkita"
+sleep 1
+echo "Wybierz interesujaca cie opcje wpisujac cyfre z poczatku wiersza:"
+echo "1.Zainstaluj caly pakiet."
+echo "2.Pokaz liste komend."
+echo "3.Wyjdz z tego menu"
+
+read odpowiedz
+
+if [ $odpowiedz -eq 1 ];
+then
+echo "Rozpoczynanie instalacji..."
+inst;pakiety;Install
+elif [ $odpowiedz -eq 2 ];
+then
+clear
+echo "Komendy:"
+echo "-----------"
+echo "1.licz"
+echo "-----------"
+elif [ $odpowiedz -eq 3 ];
+then
+echo "Wychodzenie..."
+sleep 0.7
+clear
+elif [ $odpowiedz -eq 2137 ];
+then
+yes "JAN PAWEL II GWALCIL MALE DZIECI!!!"
+else
+echo "Co?"
+echo "Nie ma takiej opcji w menu!"
+
+fi
+
